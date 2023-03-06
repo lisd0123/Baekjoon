b41a517e62d410f3e401d64da8f4768589931894
lisd0123 <124160123+lisd0123@users.noreply.github.com>
2023-03-07 00:04:35 +0900

    [Bronze V] Title: A+B - 3, Time: 240 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		
+		/* 10950 */
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int i = 1;
+		
+		for(i = 1; i < t + 1; i++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a + b);
+		}
+		
+	}
+}
+
