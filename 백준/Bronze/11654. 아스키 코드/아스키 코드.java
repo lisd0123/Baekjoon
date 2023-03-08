4a9a4b5afcf33fc67e43ee92df7b1061927de86b
lisd0123 <124160123+lisd0123@users.noreply.github.com>
2023-03-08 17:14:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	
+		/* 11654 Scanner */
+//		Scanner sc = new Scanner(System.in);
+//	    int ch = sc.next().charAt(0);
+//		System.out.println(ch);
+		
+//		/* 11654 System.in */
+		int a = System.in.read();
+		System.out.println(a);
+			
+	}
+}
+
