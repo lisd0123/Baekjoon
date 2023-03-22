6846265482da11f041c09237e52dd949dd8200bb
lisd0123 <124160123+lisd0123@users.noreply.github.com>
2023-03-22 12:39:29 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int count  = 0;
+    static int findX;
+    static int findY;
+    static boolean find = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st  = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        findY = Integer.parseInt(st.nextToken());      
+        findX = Integer.parseInt(st.nextToken());      
+        int size = (int) Math.pow(2, N);
+
+        partition(0, 0, size);
+        System.out.println(count-1);	
+    }
+
+    private static void partition(int startX, int startY, int size) {
+        if(size <= 2){
+            findXY(startX, startY);
+            return;
+        }
+
+        int half = size/2;
+        boolean left =          startX <= findX && findX < startX + half;
+        boolean right =  startX + half <= findX && findX < startX + size;
+        boolean top =           startY <= findY && findY < startY + half;
+        boolean bottom = startY + half <= findY && findY < startY + size;
+
+
+        if(left && top){
+            partition(startX, startY, half);
+            
+        }else if(right && top){
+            count = count + ((half * half) * 1);
+            partition(startX+half, startY, half);
+            
+        }else if(left && bottom){
+            count = count + ((half * half) * 2);
+            partition(startX, startY+half, half);
+            
+        }else if(right && bottom){
+            count = count + ((half * half) * 3);
+            partition(startX+half, startY+half, half);
+        }
+
+    }
+
+
+    private static boolean findXY(int startX, int startY){
+
+        for (int j = startY; j < startY + 2; j++) {     
+            for (int i = startX; i < startX+2; i++) {
+                count++;
+                if (i == findX && j == findY) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
